6cc7ccd50ce1ecbe9211392981c4d02e25ea1700
Marie-ClaireBeaulieu <marie-claire.beaulieu@tufts.edu>
2016-01-09 15:26:30 -0500

    updating pages

---
+++ css/Untitled1.js
@@ -0,0 +1,36 @@
+var x={
+  "@context": "http://www.w3.org/ns/oa-context-20130208.json",
+  "@id": "http://data.perseus.org/collections/urn:cite:perseus:pdljann.52209b217a7e013356640a092298b603",
+  "annotatedBy": {
+    "@type": "foaf:Person",
+    "@id": "http://sosol.perseids.org/sosol/users/Marie-Claire%20Beaulieu"
+  },
+  "@type": "oa:Annotation",
+  "dcterms:source": "https://hypothes.is/api/annotations/kYl8s2BgQomSA6b-6RmqPg",
+  "dcterms:title": "http://pleiades.stoa.org/places/541138#this identifies Thebes as place in urn:cts:pdlrefwk:viaf88890045.003.perseus-eng1:A.antigone_1",
+  "annotatedAt": "2015-04-07T16:03:38.807314+00:00",
+  "motivatedBy": "oa:identifying",
+  "serializedBy": {
+    "@id": "https://hypothes.is",
+    "@type": "prov:SoftwareAgent"
+  },
+  "hasTarget": {
+    "@id": "http://data.perseus.org/collections/urn:cite:perseus:pdljann.52209b217a7e013356640a092298b603#target-1",
+    "@type": "oa:SpecificResource",
+    "hasSelector": {
+      "@id": "http://data.perseus.org/collections/urn:cite:perseus:pdljann.52209b217a7e013356640a092298b603#target-1-sel-1",
+      "@type": "oa:TextQuoteSelector",
+      "exact": "Thebes",
+      "prefix": "s eyes, and was obliged to quit",
+      "suffix": ", he went to Attica guided and a"
+    },
+    "hasSource": {
+      "@id": "urn:cts:pdlrefwk:viaf88890045.003.perseus-eng1:A.antigone_1"
+    }
+  },
+  "hasBody": [
+    {
+      "@id": "http://pleiades.stoa.org/places/541138#this"
+    }
+  ]
+}
